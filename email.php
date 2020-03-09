82ef1735262fb39e6269d51d8348e610b9243e5b
Olli Perikanta <33216236+OlliPerikanta@users.noreply.github.com>
2020-03-09 12:50:13 +0200

    Update and rename index.php to email.php

--- email.php
+++ email.php
@@ -17,22 +17,3 @@ if(isset($_POST['submit'])){
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     }
 ?>
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Form submission</title>
-</head>
-
-<body>
-
-    <form action="" method="post">
-        First Name: <input type="text" name="first_name"><br> Last Name: <input type="text" name="last_name"><br> Email: <input type="text" name="email"><br> Message:
-        <br><textarea rows="5" name="message" cols="30"></textarea><br>
-        <input type="submit" name="submit" value="Submit">
-    </form>
-
-</body>
-
-</html>
